42b7e7764a08dc5a4e9162fd25d8b52f0dfa1f9c
dahyeMin <sol_1829@naver.com>
2020-12-29 00:33:21 +0900

    #4.0 Fetching Movies from API / axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
   state = {
     isLoading: true,
     movies: []
   };
-componentDidMount(){
-  setTimeout(() => {
-    this.setState({isLoading: false});
-    //setState를 사용할 때 state안에 default값들을 선언 할 필요는 없다
-  }, 6000);
-}
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
   //render 하면 호출되는 Life cycle method => componetDidMount
   render(){
     const { isLoading } = this.state;
     return (
       <div>{isLoading ? "Loading..." : "We are ready"}</div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
+  
+  //axios는 fetch 위에 있는 작은 layer라고 생각하면됨..
+  //axios는 느릴 수 있기 때문에 componentdidmount에 함수가 끝날때까지 시간이 걸린다는걸 알려야함
