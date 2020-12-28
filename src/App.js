42667d39714aec088670053721985a528c8ab4ce
dahyeMin <sol_1829@naver.com>
2020-12-29 00:00:50 +0900

    #3.3 Planning the Movie Component ~setState

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 class App extends React.Component{
-
-  // constructor(props){
-  //   super(props);
-  //   console.log("hello");
-  // }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  }; 
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  }; 
-  componentDidMount(){
-    console.log("Component rendered");
-  }
-  componentDidUpdate(){
-    console.log("I just update");    
-  }
-  componentWillUnmount(){//거의 안 쓰이는데... 니꼬쌤은 동작도 못 찾았다
-    //아!!! vs코드에서 저장을 눌러서 자동으로 로컬페이지가 끊겼다가 켜질때 호출됨
-    console.log("Goodbye, Cruel world");
-  }
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({isLoading: false});
+    //setState를 사용할 때 state안에 default값들을 선언 할 필요는 없다
+  }, 6000);
+}
+  //render 하면 호출되는 Life cycle method => componetDidMount
   render(){
-    console.log("Im rendering");
+    const { isLoading } = this.state;
     return (
-      <div>
-        <h1>The number is: { this.state.count }</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div> 
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
     );
   }
 }
-//test
-//render를 돌고 component rendered > 업데이트 실행이 되면 렌더링> update
-//
+
 export default App;
