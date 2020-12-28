73d578cbfe2115938be6b76d5f49bdea347c06fa
dahyeMin <sol_1829@naver.com>
2020-12-28 22:31:42 +0900

    #3.2 Component Life Cycle modify..

--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// 위의 import를 하지 않으면 리액트는 여기 jsx가 있는 컴포넌트를 사용하는 것을 이해하지 못함
-// JSX는 자바스크립트 안의 html
-
-function Potato(){
-    return <h3>I love potato .. Potato component</h3>;
-}
-
-export default Potato;
