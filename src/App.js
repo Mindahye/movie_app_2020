7128f8505c03095e73331e931489b3620de69ba0
dahyeMin <sol_1829@naver.com>
2021-01-03 22:03:51 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
+import './App.css';
 
 function App() {
   return (
     <HashRouter>
+      {/* BrowserRouter로 쓸 수도 있는데 github에서 쓰기 별로라고 함.. 알아볼것 */}
+      <Navigation />
+      {/* 네비게이션은 라우터 안에 있어야 함  Link to를 사용하려면..!
+      footer는 없어두 되겠지.. link to로 변경되는게 아니니까*/}
       <Route path="/" exact={true} component={Home} />
+      {/* exact는 path가 / 일때만 본다 */}
       <Route path="/about" component={About} />
     </HashRouter>
   );
